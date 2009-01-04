d8071420812fee4c2de6a34a7faa40e5e5f09eaf
don_lucas <don_lucas@41062d70-b26d-460a-9302-f438a8930db8>
2009-01-04 23:31:01 +0000

    Added support for compiling with VS2005 and cleaned up the comments referring to  VS2005.
    git-svn-id: https://216.105.38.17/p/tinycad/code/TinyCAD/trunk@42 41062d70-b26d-460a-9302-f438a8930db8

--- src/StdAfx.h
+++ src/StdAfx.h
@@ -21,7 +21,7 @@
 #define AFX_STDAFX_H__7554172E_EEA9_40FA_8A4A_C9D910CB873F__INCLUDED_
 
 #if _MSC_VER > 1000
-#pragma once
+    #pragma once
 #endif // _MSC_VER > 1000
 
 // Visual Studio 2003:  _MSC_VER = 1300 through 1399
@@ -32,15 +32,19 @@
 	#pragma message("Error:  TinyCAD is not supported or tested with non-Microsoft compilers")
 #elif _MSC_VER < 1300
 	#define USE_VS2003
-	#pragma message("Warning:  Microsoft Visual C++ compilers older than Visual Studio .NET 2003 are not supported.")
-#elif _MSC_VER < 1500
-	//This source can be compiled with either VS2003 or VS2005 with this setting
+	#pragma message("Warning:  Microsoft Visual C++ compilers older than Visual Studio .NET 2003 are no longer supported or tested.")
+#elif _MSC_VER < 1400
+	//This source can be compiled with either VS2003 with this setting
 	#define USE_VS2003
-	#pragma message("Compiling TinyCAD with Visual Studio .NET 2003 or Visual Studio .NET 2005")
+	#pragma message("Compiling TinyCAD with Visual Studio .NET 2003")
+#elif _MSC_VER < 1500
+	//This source can be compiled with VS2005 with this setting
+	#define USE_VS2008
+	#pragma message("Compiling TinyCAD with Visual Studio .NET 2005.  \"Safe\" functions will be used.")
 #elif _MSC_VER < 1600
 	//This source can be compiled with VS2008 with this setting
 	#define USE_VS2008
-	#pragma message("Compiling TinyCAD with Visual Studio .NET 2008")
+	#pragma message("Compiling TinyCAD with Visual Studio .NET 2008.  \"Safe\" functions will be used.")
 #else
 	//This source has not been tested with a Visual Studio C++ compiler newer than 2008
 	#define USE_VS2008
@@ -56,8 +60,8 @@
 	//by appending an "_s" onto the end of the non-safe version (i.e., the VS2003 version) of the function.
 	//The following macros along with specific conditional #ifdef's in some places permits TinyCAD
 	//to be compiled under VS2003 or VS2008.  I have not tested TinyCAD under VS2005, but this source
-	//is likely to be compatible with the VS2003 setting because VS2005 does not yet support the "safe"
-	//versions of the MSVCRT libraries and the current TinyCAD source is already compatible with the
+	//is likely to be compatible with the VS2008 setting because VS2005 was the first version to implement
+    //the "safe" versions of the string functions and the current TinyCAD source is already compatible with the
 	//newer stricter for loop variable definition scope and other newer ISO C++ strict requirements.
 
 	#define _stprintf_s _stprintf		/* just a simple rename to use the older function name - no argument manipulation required */
@@ -78,7 +82,8 @@
 
 //I am not sure why Visual Studio 2008 deprecated the CDaoDatabase class, but it
 //may have to do with the fact that there is no 64 bit processor support for it.
-//This will disable the warnings, for now
+//This will disable the warnings, for now, but until something newer is implemented
+//to take its place, TinyCAD will be unable to support 64 bit architectures or Windows Vista.
 #define _AFX_DISABLE_DEPRECATED 1
 
 #define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
@@ -87,8 +92,9 @@
 #include <afxext.h>         // MFC extensions
 #include <afxdisp.h>        // MFC Automation classes
 #include <afxdtctl.h>		// MFC support for Internet Explorer 4 Common Controls
+
 #ifndef _AFX_NO_AFXCMN_SUPPORT
-#include <afxcmn.h>			// MFC support for Windows Common Controls
+    #include <afxcmn.h>		// MFC support for Windows Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
 #include <tchar.h>
