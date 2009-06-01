7a328886300d85ee21bae51736c161c2acf21c43
don_lucas <don_lucas@41062d70-b26d-460a-9302-f438a8930db8>
2009-06-01 04:05:55 +0000

    The following new files from another open source author will help TinyCAD to better manage multiple monitors in future versions.  They are presently not invoked anywhere in the TinyCAD code.  They were brought into TinyCAD to support fixing this bug involving multiple monitors, but it turned out that they weren't required to fix this particular bug, but it is now apparent that TinyCAD is using obsolete classes to obtain the horizontal and vertical resolution of the display.  These obsolete classes will only work correctly when TinyCAD is located on the primary monitor and has no knowledge of a possible secondary monitor.  All future changes to TinyCAD that are dependent on the device context should use these classes instead of the older classes. Issue/Tracker #2790772
    git-svn-id: https://216.105.38.17/p/tinycad/code/TinyCAD/trunk@239 41062d70-b26d-460a-9302-f438a8930db8

---
+++ src/MonitorDC.h
@@ -0,0 +1,32 @@
+//////////////////////////////////////////////////
+// CMonitor - wrapper to Win32 multi-monitor API
+//
+// Author: Donald Kackman
+// Email:  dkackman_2000@yahoo.com
+// Copyright 2002, Donald Kackman
+//
+// You may freely use or modify this code provided this
+// Copyright is included in all derived versions.
+//
+///////////////////////////////////////////////////
+
+//
+//David Campbell's article
+//How to Exploit Multiple Monitor Support in Memphis and Windows NT 5.0
+//is very helpful for multimonitor api calls
+//http://www.microsoft.com/msj/defaultframe.asp?page=/msj/0697/monitor/monitor.htm&nav=/msj/0697/newnav.htm
+//
+#pragma once
+
+class CMonitor;
+
+// CMonitorDC command target
+
+class CMonitorDC : public CDC
+{
+public:
+	CMonitorDC( CMonitor* pMonitor );
+	virtual ~CMonitorDC();
+};
+
+
