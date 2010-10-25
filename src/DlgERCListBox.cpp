12367f63324a9d7607ba2fdbf8265ea891b794eb
don_lucas <don_lucas@41062d70-b26d-460a-9302-f438a8930db8>
2010-10-25 01:50:31 +0000

    Merged from Mark Langezaal's branch "b_several_fixes_and_enhancements".  Original comments were: Fix for "The ERC error list will not show up again when the window is closed by pressing ESC." Issue/Tracker #3094447
    git-svn-id: https://216.105.38.17/p/tinycad/code/TinyCAD/trunk@450 41062d70-b26d-460a-9302-f438a8930db8

--- src/DlgERCListBox.cpp
+++ src/DlgERCListBox.cpp
@@ -40,8 +40,8 @@ END_MESSAGE_MAP()
 
 CDlgERCListBox::CDlgERCListBox()
 {
-  open=FALSE;
-  stop=FALSE;
+	open=FALSE;
+	stop=FALSE;
 }
 
 void CDlgERCListBox::Open(CMultiSheetDoc *pDesign, CTinyCadView *pView)
@@ -49,69 +49,76 @@ void CDlgERCListBox::Open(CMultiSheetDoc *pDesign, CTinyCadView *pView)
 	m_pDesign = pDesign;
 	m_pView = pView;
 
-  if (!open) {
-  	Create(IDD_ERCLIST,AfxGetMainWnd());
+	if (!open) {
+		Create(IDD_ERCLIST,AfxGetMainWnd());
 
-	if (!m_pView) {
-		CWnd *wnd = GetDlgItem (IDOK2);
-		wnd->ShowWindow(SW_HIDE);
-    }
-  }
-  theListBox = (CListBox *)GetDlgItem(ERCLIST_LIST);
-  theListBox->ResetContent();
+		if (!m_pView) {
+			CWnd *wnd = GetDlgItem (IDOK2);
+			wnd->ShowWindow(SW_HIDE);
+		}
+	}
+	theListBox = (CListBox *)GetDlgItem(ERCLIST_LIST);
+	theListBox->ResetContent();
 
-  open = TRUE;
-  stop = FALSE;
+	open = TRUE;
+	stop = FALSE;
 }
 
 // Close the dialog window 
 void CDlgERCListBox::Close()
 {
-  if (open) {
-	DestroyWindow();
-	// Now remove all the errors from the design
-	  for (int i = 0; i < m_pDesign->GetNumberOfSheets(); i++)
-	  {
-		m_pDesign->GetSheet(i)->DeleteErrors();
+	if (open) {
+		DestroyWindow();
+		// Remove all previous errors from the design
+		for (int i = 0; i < m_pDesign->GetNumberOfSheets(); i++)
+		{
+			m_pDesign->GetSheet(i)->DeleteErrors();
+		}
 		open = FALSE;
-	  }
-  }
+	}
 }
 
-// Close the dialog window 
+// Default cancel button must close the dialog window 
+void CDlgERCListBox::OnCancel()
+{
+	CDlgERCListBox::Close();
+}
+
+
+// Re-check for ERC errors
 void CDlgERCListBox::ReCheck()
 {
-  if (open) {
-	// Now remove all the errors from the design
-	  for (int i = 0; i < m_pDesign->GetNumberOfSheets(); i++)
-	  {
-		 m_pDesign->GetSheet(i)->DeleteErrors();
-	  }
+	if (open) {
+		// Remove all previous errors from the design
+		for (int i = 0; i < m_pDesign->GetNumberOfSheets(); i++)
+		{
+			m_pDesign->GetSheet(i)->DeleteErrors();
+		}
 
-	  if (m_pView)
-	  {
-		 m_pView->DoSpecialCheck();
-	  }
-  }
+		if (m_pView)
+		{
+			m_pView->DoSpecialCheck();
+		}
+	}
 }
 
 
 void CDlgERCListBox::AddString(CString NewString)
 {
-  if (open) {
-	theListBox = (CListBox *)GetDlgItem(ERCLIST_LIST);
-	theListBox->AddString(NewString);
-  }
+	if (open) {
+		theListBox = (CListBox *)GetDlgItem(ERCLIST_LIST);
+		theListBox->AddString(NewString);
+	}
 }
 
 void CDlgERCListBox::SetSelect(int NewSelect)
 {
-  if (open) {
-	stop = TRUE;
-	theListBox = (CListBox *)GetDlgItem(ERCLIST_LIST);
-	theListBox->SetCurSel(NewSelect);
-	stop = FALSE;
-  }
+	if (open) {
+		stop = TRUE;
+		theListBox = (CListBox *)GetDlgItem(ERCLIST_LIST);
+		theListBox->SetCurSel(NewSelect);
+		stop = FALSE;
+	}
 }
 
 /**
@@ -119,47 +126,47 @@ void CDlgERCListBox::SetSelect(int NewSelect)
  */
 void CDlgERCListBox::OnClick()
 {
-  // Do we notice this?
-  if (stop) return;
+	// Do we notice this?
+	if (stop) return;
 
-  // Get the item which has been selected
-  int WhichItem;
-  theListBox = (CListBox *)GetDlgItem(ERCLIST_LIST);
-  if ((WhichItem = theListBox->GetCurSel())==LB_ERR) {
-	  return;
-  }
-
-  // Now select the item in the current design (if we can...)
-  m_pDesign->GetCurrentSheet()->UnSelect();
-
-  for (int sheetIndex = 0; sheetIndex < m_pDesign->GetNumberOfSheets(); sheetIndex++)
-  {
-	drawingIterator it = m_pDesign->GetSheet(sheetIndex)->GetDrawingBegin();
-	while (it != m_pDesign->GetSheet(sheetIndex)->GetDrawingEnd()) 
-	{
-		CDrawingObject *ercObject = *it;
-
-		if (ercObject->GetType()==xError && static_cast<CDrawError*>(ercObject)->GetErrorNumber() == WhichItem)
-		{
-			//The next statement displays the page with the ERC marker centered in view and at a zoom factor that is still under the user's control
-			m_pDesign->SelectERCSheetView( sheetIndex, ercObject );	//This displays a view centered around the ERC marker.
-
-			//The next statement selects the error object so that it turns from a heavy bordered circle
-			//in brown into a heavy bordered circle in bright red
-			m_pDesign->GetCurrentSheet()->Select( ercObject );	//This selects the object
-
-			//Sometimes there is more than one ERC marker at the same spot - it won't appear highlighted 
-			//to the user unless it is on top!
-			//Note:	Because BringToFront() changes the ordering of objects in the drawing, this action will
-			//invalidate the iterator being used to traverse this list!  You must quit iterating this list
-			//after bringing this object to the front!
-			m_pDesign->GetCurrentSheet()->BringToFront();
-
-			ercObject->Display();	//This draws the object and also updates the page name selection tab
-			break;	//While logically appropriate, this statement is also required to avoid crashing when incrementing the iterator which is now invalid
-		}
-
-		++ it;
+	// Get the item which has been selected
+	int WhichItem;
+	theListBox = (CListBox *)GetDlgItem(ERCLIST_LIST);
+	if ((WhichItem = theListBox->GetCurSel())==LB_ERR) {
+		return;
 	}
-  }
+
+	// Now select the item in the current design (if we can...)
+	m_pDesign->GetCurrentSheet()->UnSelect();
+
+	for (int sheetIndex = 0; sheetIndex < m_pDesign->GetNumberOfSheets(); sheetIndex++)
+	{
+		drawingIterator it = m_pDesign->GetSheet(sheetIndex)->GetDrawingBegin();
+		while (it != m_pDesign->GetSheet(sheetIndex)->GetDrawingEnd()) 
+		{
+			CDrawingObject *ercObject = *it;
+
+			if (ercObject->GetType()==xError && static_cast<CDrawError*>(ercObject)->GetErrorNumber() == WhichItem)
+			{
+				//The next statement displays the page with the ERC marker centered in view and at a zoom factor that is still under the user's control
+				m_pDesign->SelectERCSheetView( sheetIndex, ercObject );	//This displays a view centered around the ERC marker.
+
+				//The next statement selects the error object so that it turns from a heavy bordered circle
+				//in brown into a heavy bordered circle in bright red
+				m_pDesign->GetCurrentSheet()->Select( ercObject );	//This selects the object
+
+				//Sometimes there is more than one ERC marker at the same spot - it won't appear highlighted 
+				//to the user unless it is on top!
+				//Note:	Because BringToFront() changes the ordering of objects in the drawing, this action will
+				//invalidate the iterator being used to traverse this list!  You must quit iterating this list
+				//after bringing this object to the front!
+				m_pDesign->GetCurrentSheet()->BringToFront();
+
+				ercObject->Display();	//This draws the object and also updates the page name selection tab
+				break;	//While logically appropriate, this statement is also required to avoid crashing when incrementing the iterator which is now invalid
+			}
+
+			++ it;
+		}
+	}
 }
