51af4a278c93fe0f378543f1c1430d171a8efc0d
Don_Lucas <Don_Lucas@41062d70-b26d-460a-9302-f438a8930db8>
2012-12-09 22:21:55 +0000

    Copied resources for File Open, File New, File Save, and File Save As from the U.K. English string table to the default string table to try to help resolve the exceptions that can occur on some systems when the local machine locale is different from U.K. English or U.S. English.  Don't know if this helps or not as I can't reproduce this problem.
    git-svn-id: https://216.105.38.17/p/tinycad/code/TinyCAD/trunk@616 41062d70-b26d-460a-9302-f438a8930db8

--- src/resource.h
+++ src/resource.h
@@ -484,7 +484,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        169
+#define _APS_NEXT_RESOURCE_VALUE        170
 #define _APS_NEXT_COMMAND_VALUE         32898
 #define _APS_NEXT_CONTROL_VALUE         1585
 #define _APS_NEXT_SYMED_VALUE           113
